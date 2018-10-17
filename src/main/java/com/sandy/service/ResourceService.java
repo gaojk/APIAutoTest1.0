d01395095f170a20d9648d8a976ee8f646be1de1
sandy <sandyforawhile@126.com>
2018-10-17 11:29:22 +0800

    add

---
+++ src/main/java/com/sandy/service/ResourceService.java
@@ -0,0 +1,15 @@
+package com.sandy.service;
+
+/**
+ * @ClassName: ResourceService
+ * @Author sandy.n.hao
+ * @Date: 2018/10/11
+ * @Version v1.0.0
+ * @Description: //TODO
+ */
+
+public interface ResourceService {
+
+
+
+}
